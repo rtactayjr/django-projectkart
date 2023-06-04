91acbf83f77b23b1557d46343b5dc72ff9b8ba30
r-tactay <rolandtactay571@gmail.com>
2023-06-04 14:55:23 +0800

    django authentication

--- kartproject/settings.py
+++ kartproject/settings.py
@@ -136,3 +136,19 @@ STATICFILES_DIRS = [
 #media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+    50: 'critical',
+}
+
+
+#SMTP CONFIGURATION
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend' 
+EMAIL_HOST = 'smtp.office365.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = 'RonTesting101@outlook.com'
+EMAIL_HOST_PASSWORD = '1qaZxsw@3edc'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'RonTesting101@outlook.com'
